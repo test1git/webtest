582d72d2a73f8aaab530fe1609f0c08f627239d7
Adam Wilczak <wilczak.adam.123@gmail.com>
2015-02-23 11:18:31 -0500

    moar tests

---
+++ server/api/thing/thing2.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var should = require('should');
+var app = require('../../app');
+var request = require('supertest');
+
+describe('GET /api/things', function() {
+
+  it('expecting 304', function(done) {
+    request(app)
+      .get('/api/things')
+      .expect(304)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+});
